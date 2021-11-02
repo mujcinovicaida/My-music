2611585181aa6d5f2d5afbb8445631a99de71ffa
mujcinovicaida <aida.mujcinovic@size.ba>
2021-11-02 15:15:12 +0100

    Dashboard(join and sign in)

--- MyMusic/src/app/app-routing.module.ts
+++ MyMusic/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { JoinComponent } from './join/join.component';
+import { PostsComponent } from './posts/posts.compoent';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path:'posts',
+    component: PostsComponent
+  },
+  {
+    path:'join',
+    component: JoinComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MyMusic/src/app/app.module.ts
+++ MyMusic/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {MatButtonModule} from '@angular/material/button';
+import { PostsComponent } from './posts/posts.compoent';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { JoinComponent } from './join/join.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PostsComponent,
+    JoinComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
